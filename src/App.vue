fb5f6c07ea4da77084774f41d7fca057ccdff47e
maybe <1013480204@qq.com>
2019-09-02 17:40:05 +0800

    2019年9月2日 17:39:57

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>mv-count-down 倒计时组件</h1>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  margin: 0px;
+  padding: 0px;
+}
+html,
+body {
+  height: 100%;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="home">
+    <mv-count-down @startCallback="countDownS_cb"
+                   @endCallback="countDownE_cb"
+                   :startTime="startTime"
+                   :endTime="endTime"
+                   :endText="endText"
+                   :dayTxt="'天'"
+                   :hourTxt="'小时'"
+                   :minutesTxt="'分钟'"
+                   :secondsTxt="'秒'"
+                   :isStart="isStart"></mv-count-down>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MvCountDown from '@/components/MvCountDown.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MvCountDown
+  },
+  data () {
+    return {
+      content: '',
+      startTime: new Date().getTime(),
+      endTime: 0,
+      endText: '',
+      isStart: false
+    }
+  },
+  created () {
+    this.setEndTime(111160);
+  },
+  methods: {
+    setEndTime (value) {
+      let curTime = new Date();
+      let newTime = new Date(curTime.setSeconds(curTime.getSeconds() + value));
+      this.endTime = newTime.getTime();
+      this.isStart = true;
+    },
+    countDownS_cb (x) {
+      console.log(x)
+    },
+    countDownE_cb (x) {
+      // this.callback();
+      console.log(x)
+    }
+  }
+}
+</script>
